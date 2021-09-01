56c24a930f3b4908b67509a5e9342f2cc361b31f
wwfia <Esse@e00>
2021-09-01 12:33:59 -0300

    Initial and last commit

---
+++ shopping-cart/src/main/java/com/dio/shoppingcart/config/RedisConfig.java
@@ -0,0 +1,29 @@
+package com.dio.shoppingcart.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
+import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.repository.configuration.EnableRedisRepositories;
+
+@Configuration
+@EnableRedisRepositories
+public class RedisConfig {
+
+    @Bean
+    public JedisConnectionFactory redisConnectionFactory() {
+
+        RedisStandaloneConfiguration config = new RedisStandaloneConfiguration("localhost", 6379);
+        return new JedisConnectionFactory(config);
+    }
+
+    @Bean
+    public RedisTemplate<String, Object> redisTemplate() {
+        RedisTemplate<String, Object> template = new RedisTemplate<>();
+        template.setConnectionFactory(redisConnectionFactory());
+        return template;
+    }
+
+
+}
---
+++ shopping-cart/src/main/java/com/dio/shoppingcart/repository/CartRepository.java
@@ -0,0 +1,10 @@
+package com.dio.shoppingcart.repository;
+
+import com.dio.shoppingcart.model.Cart;
+import org.springframework.data.repository.CrudRepository;
+
+public interface CartRepository extends CrudRepository<Cart, Integer> {
+
+    Cart findByCustomerId(Integer customerId);
+
+}
